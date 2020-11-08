31dd95acd975ef3a9a03939d1196212734defd0d
Joseph Verron <joseph.verron@gmail.com>
2020-11-09 00:29:12 +0100

    add p5

--- test/p99/core_test.clj
+++ test/p99/core_test.clj
@@ -22,12 +22,11 @@
 (deftest can-intro-to-lists
   (is (= intro-to-lists-solution '(:a :b :c))))
 
-; ;; problem 5
-; (deftest can-lists-conj
-;   (is (= lists-conj-solution (conj '(2 3 4) 1)))
-;   (is (= lists-conj-solution (conj '(3 4) 2 1))))
-;
-;
+;; problem 5
+(deftest can-lists-conj
+  (is (= lists-conj-solution (conj '(2 3 4) 1)))
+  (is (= lists-conj-solution (conj '(3 4) 2 1))))
+
 ; ;; problem 6
 ; (deftest can-intro-to-vectors
 ;   (is (= [intro-to-vectors-solution] (list :a :b :c) (vec '(:a :b :c)) (vector :a :b :c))))
