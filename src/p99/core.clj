058b8e4c444e92359ae57da39992a4dd7253d17f
Joseph Verron <joseph.verron@gmail.com>
2020-11-08 23:42:41 +0100

    first method

--- src/p99/core.clj
+++ src/p99/core.clj
@@ -12,12 +12,7 @@
 ;;; see it online.
 
 ;; problem 1 (Elementary)
-(def nothing-but-the-truth-solution
-  ;; This is a clojure form. Enter a value which will make the form evaluate to
-  ;; true. Don't over think it! If you are confused, see the getting started
-  ;; page. Hint: true is equal to true.
-  nil)
-
+(def nothing-but-the-truth-solution true)
 
 ;; problem 2 (Elementary)
 (def simple-math-solution
